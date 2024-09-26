09ff3b36548ec56dea3687bc9e02399fd406e127
root <root@Hicham1S>
2024-09-26 13:43:25 +0300

    added

--- src/build_in_function/ft_echo.c
+++ src/build_in_function/ft_echo.c
@@ -3,6 +3,8 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include "../libft/libft.h"
+#include "../parsing/parsing.h"
+
 
 int is_valid_n_flag(char *arg) {
     int i = 1;
@@ -16,42 +18,25 @@ int is_valid_n_flag(char *arg) {
     return 1;
 }
 
-char *get_env_value(char *var, char **envp) {
-    int i = 0;
-    int var_len = strlen(var);
-    while (envp[i]) {
-        if (strncmp(envp[i], var, var_len) == 0 && envp[i][var_len] == '=')
-            return strdup(&envp[i][var_len + 1]);
-        i++;
-    }
-    return NULL;
-}
-
 void handle_echo(char **args, char **envp) {
     int i = 1;
     int newline = 1;
+    char *expanded_arg;
 
-    if (args[i] && is_valid_n_flag(args[i])) {
-        newline = 0;
+    if (args[i] && is_valid_n_flag(args[i]))
+    {
+      newline = 0;
         i++;
     }
-
-    while (args[i]) {
-        if (args[i][0] == '$')
-		{
-            char *env_value = get_env_value(&args[i][1], envp);
-            if (env_value) {
-                printf("%s", env_value);
-                free(env_value);
-            }
-        }
-		else
-            printf("%s", args[i]);
+    while (args[i])
+    {
+        expanded_arg = expand_env_var(args[i], envp, 0);
+        printf("%s", expanded_arg);
         if (args[i + 1])
             printf(" ");
+        free(expanded_arg);
         i++;
     }
-
     if (newline)
         printf("\n");
 }
