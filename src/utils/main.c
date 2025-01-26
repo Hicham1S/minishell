c979518f8a197591a1d9ccb9e5eb8274fedf5b02
Danny Awwad <dani.awwad25@gmail.com>
2025-01-26 11:29:27 +0200

    added a more organized main

--- src/utils/main.c
+++ src/utils/main.c
@@ -1,15 +1,30 @@
 #include "../../includes/minishell.h"
 
-int main(int argc, char **argv, char **envp)
+t_cmd	*input_to_cmd(char *input, t_env *env)
+{
+	t_token	*tokens;
+	t_cmd	*cmd;
+
+	cmd = NULL;
+	tokens = init_tokens(input);
+	if (redir_check(tokens, env))
+	{
+		replace_tokens_with_env(env, tokens);
+		merge_tokens(&tokens);
+		printf("\n\n\nParsed Tokens with Replaced Variables:\n");
+		print_tokens(tokens);
+		cmd = init_cmd(tokens);
+		print_cmd(cmd);
+	}
+	if (tokens)
+		free_tokens(&tokens);
+	return (cmd);
+}
+
+void	readline_loop(t_env *env)
 {
 	char	*input;
-	t_token	*tokens;
-	t_env	*env;
 	t_cmd	*cmd;
-	
-	(void)argc;
-	(void)argv;
-	env = init_env(envp);
 
 	while (1)
 	{
@@ -17,31 +32,31 @@ int main(int argc, char **argv, char **envp)
 		if (!input)
 		{
 			printf("exit\n");
-			break;
+			break ;
 		}
 		if (*input)
 			add_history(input);
 		if (unmatched_quotes(input))
 		{
-			printf("Error: Unmatched quotes\n");
+			printf("Error: Mismatched quotes\n");
 			free(input);
-			continue;
+			continue ;
 		}
-		tokens = init_tokens(input);
-		if (redir_check(tokens, env))
-		{
-			replace_tokens_with_env(env, tokens);
-			merge_tokens(&tokens);
-			printf("\n\n\nParsed Tokens with Replaced Variables:\n");
-			print_tokens(tokens);
-			cmd = init_cmd(tokens);
-			print_cmd(cmd);
-			free_cmd(cmd);
-		}
-		if (tokens)
-			free_tokens(&tokens);
+		cmd = input_to_cmd(input, env);
+		free_cmd(cmd);
 		free(input);
 	}
-	free(env);
+}
+
+int	main(int argc, char **argv, char **envp)
+{
+	t_minishell	*minishell;
+
+	(void)argc;
+	(void)argv;
+	minishell = (t_minishell *)malloc(sizeof(t_minishell));
+	minishell->envs = init_env(envp);
+	readline_loop(minishell->envs);
+	free(minishell->envs);
 	return (0);
 }
