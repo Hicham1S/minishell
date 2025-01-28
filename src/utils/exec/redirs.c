3993cb73ac5dddcb8e383b54ac941b13b6618c75
Hicham <sarajhicham@gmail.com>
2025-01-28 14:50:06 +0200

    starting with the redirs

--- src/utils/exec/redirs.c
+++ src/utils/exec/redirs.c
@@ -12,6 +12,40 @@
 
 #include "minishell.h"
 
+static bool	redir_heredoc(char *delimiter, t_cmd *cmd)
+{
+	char	*line;
+
+	cmd.infile = open(HEREDOC_FILE, O_WRONLY)
+}
+
+bool	init_redirs(char **tokens, size_t i, t_cmd *cmd)
+{
+	if (tokens[i][0] == '>')
+	{
+		if (cmd->outfile > 2)
+			close(cmd->outfile);
+		if (tokens[i][1] == '>')
+			cmd->outfile = open(tokens[i + 1], O_WRONLY | O_CREAT
+					| O_APPEND, 0644);
+		else
+			cmd->outfile = open(tokens[i + 1], O_WRONLY | O_CREAT
+					| O_TRUNC, 0644);
+		if (cmd->outfile < 0)	
+			return (perror("minishell"), false);
+	}
+	else if (tokens[i][0] == '<')
+	{
+		if (cmd.infile > 2)
+			close(cmd.infile);
+		if (tokens[i][1] == '<')
+			return (redir_heredoc(tokens[i + 1], cmd));
+		cmd.infile = open(tokens[i + 1], O_RDONLY);
+		if (cmd.infile < 0)
+			return (perror("minishell"), false);
+	}
+	return (true);
+}
 void	redirs(t_cmd *cmd)
 {
 	if (cmd->infile > 0)
